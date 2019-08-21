416435b425798bdf72180ea93a0c047ac13bcb83
spooky0117 <sp00ky@hotmail.co.jp>
2019-08-21 23:35:40 +0900

    変更
    - Microsoft Edge(IE)関連 css追加
    - キャンセルボタン周り変更

--- js/input-ex.js
+++ js/input-ex.js
@@ -7,48 +7,53 @@ $(document).on('focusout', '.input-ex', function() {
   }
 });
 $(document).on('input', '.input-ex', function() {
-  if ($(this).val().length > 0) {
-    $(this).parent().find('.cancel-btn').addClass('active');
+  if (this.value) {
+    $(this).siblings('.label-wrapper').find('.cancel-btn').addClass('active');
   } else {
-    $(this).parent().find('.cancel-btn').removeClass('active');
+    $(this).siblings('.label-wrapper').find('.cancel-btn').removeClass('active');
   }
 });
 $(document).on('click', '.input-ex-wrapper .cancel-btn', function() {
+  $(this).parent().siblings('.input-ex').val('');
+  $(this).parent().removeClass('active');
   $(this).removeClass('active');
-  var $input = $(this).parent().siblings('.input-ex');
-  $input.val('');
-  $input.focus();
 });
+$(document).on({
+  "mouseenter": function() {
+    $(this).children('span').addClass('hover');
+  },
+  "mouseleave": function() {
+    $(this).children('span').removeClass('hover');
+  }
+}, '.input-ex-wrapper .cancel-btn');
+
 $(function() {
   $('input.input-ex').each(function() {
-    var type    = $(this).attr('type'),
-        name    = $(this).attr('name'),
-        size    = $(this).attr('size'),
-        maxlen  = $(this).attr('maxlength'),
-        id      = $(this).attr('id'),
-        classes = $(this).attr('class'),
-        label   = $(this).attr('label-text'),
-        cancel  = $(this).attr('cancel-text');
-    var template  = '<div class="label-wrapper"><label';
+    var id         = $(this).attr('id');
+    var labelText  = $(this).data('label-text'),
+        labelFixed = $(this).data('label-fixed'),
+        cancelBtn  = $(this).data('cancel-btn');
+    var template  = '<div class="label-wrapper';
+        template += (labelFixed === true) ? ' fixed' : '';
+        template += '"><label';
         template += (typeof id !== 'undefined') ? ' for="' + id + '"' : '';
-        template += '>' + label + '</label><span class="cancel-btn">';
-        template += (typeof cancel !== 'undefined') ? cancel : '<i class="far fa-times-circle"></i>';
-        template += '</span></div>';
+        template += '>' + labelText + '</label>';
+        template += (cancelBtn !== false) ? '<span class="cancel-btn"><span>&times;</span></span>' : '';
+        template += '</div>';
     $(this).wrap('<div class="input-ex-wrapper input"></div>');
     $(this).after(template);
   });
   $('textarea.input-ex').each(function() {
-    var name    = $(this).attr('name'),
-        cols    = $(this).attr('cols'),
-        rows    = $(this).attr('rows'),
-        id      = $(this).attr('id'),
-        classes = $(this).attr('class'),
-        label   = $(this).attr('label-text'),
-        cancel  = $(this).attr('cancel-text');
-    var template  = '<div class="label-wrapper"><label';
+    var id        = $(this).attr('id');
+    var labelText = $(this).data('label-text'),
+        labelFixed = $(this).data('label-fixed'),
+        cancelBtn     = $(this).data('cancel-btn');
+    var template  = '<div class="label-wrapper';
+        template += (labelFixed === true) ? ' fixed' : '';
+        template += '"><label';
         template += (typeof id !== 'undefined') ? ' for="' + id + '"' : '';
-        template += '>' + label + '</label>';
-        template += '<span class="cancel-btn"><i class="far fa-times-circle"></i></span>';
+        template += '>' + labelText + '</label>';
+        template += (cancelBtn !== false) ? '<span class="cancel-btn"><span>&times;</span></span>' : '';
         template += '</div>';
         $(this).wrap('<div class="input-ex-wrapper textarea"></div>');
         $(this).after(template);
